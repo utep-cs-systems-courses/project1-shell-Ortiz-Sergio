9dd7496448b448300551cd2048c725fe3c75508c
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-09-04 15:23:14 -0600

    adds basic fork functionality, copied from demo

---
+++ shell/sergio-shell.py
@@ -0,0 +1,15 @@
+import os, sys
+
+pid = os.getpid()
+
+os.write(1, ("About to fork (pid:%d)\n" %pid).encode())
+
+rc = os.fork()
+
+if rc < 0:
+    os.write(2, ("fork failed, returning %d\n" % rc).encode())
+    sys.exit(1)
+elif rc == 0:
+    os.write(1, ("I am child. My pid=%d. Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+else:
+    os.write(1, ("I am parent. My pid=%d. Child's pid=%d\n" % (pid, rc)).encode())
