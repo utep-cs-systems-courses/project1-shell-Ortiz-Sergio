facc2561c0019be4897229ce78e789ef3482c7f5
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-09-04 15:59:29 -0600

    takes in user input and keeps forking until user wants

--- shell/sergio-shell.py
+++ shell/sergio-shell.py
@@ -1,15 +1,29 @@
-import os, sys
+import os, sys, time, re
 
-pid = os.getpid()
+def my_shell():
+    pid = os.getpid()
 
-os.write(1, ("About to fork (pid:%d)\n" %pid).encode())
+    os.write(1, ("About to fork (pid:%d)\n" %pid).encode())
 
-rc = os.fork()
+    rc = os.fork()
 
-if rc < 0:
-    os.write(2, ("fork failed, returning %d\n" % rc).encode())
-    sys.exit(1)
-elif rc == 0:
-    os.write(1, ("I am child. My pid=%d. Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-else:
-    os.write(1, ("I am parent. My pid=%d. Child's pid=%d\n" % (pid, rc)).encode())
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    elif rc == 0:
+        os.write(1, ("I am child. My pid=%d. Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+    else:
+        os.write(1, ("I am parent. My pid=%d. Child's pid=%d\n" % (pid, rc)).encode())
+
+def test():
+    print("test print")
+
+
+print("Welcome to this amazing shell! Enter your command or -1 to exit")
+user_input = input("$ ")
+
+while(user_input != '-1'):
+    my_shell()
+    user_input = input("$ ")
+
+print("Thank you for using this amazing shell!")
