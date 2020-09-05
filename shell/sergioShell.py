a957e231a98b7f2579580f6a624cf5b0e839c55f
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-09-05 14:18:38 -0600

    factors in user input, user can fork process as long as they want

---
+++ shell/sergioShell.py
@@ -0,0 +1,34 @@
+import os, sys, time, re
+
+def my_shell():
+    pid = os.getpid()
+
+    os.write(1, ("About to fork (pid:%d)\n" %pid).encode())
+
+    rc = os.fork()
+
+    if rc < 0:
+        os.write(2, ("fork failed, returning %d\n" % rc).encode())
+        sys.exit(1)
+    elif rc == 0:
+        os.write(1, ("I am child. My pid=%d. Parent's pid=%d\n" % (os.getpid(), pid)).encode())
+        time.sleep(1)
+        os.write(1, "Child  ....terminating now with exit code 0\n".encode())
+        sys.exit(0)
+    else:
+        os.write(1, ("I am parent. My pid=%d. Child's pid=%d\n" % (pid, rc)).encode())
+        childPidCode = os.wait()
+        os.write(1, ("Parent: Child %d terminated with exit code %d\n" %childPidCode).encode())
+
+def main() :
+    print("Welcome to Sergio Shell! Type in your command or -1 to exit")
+    user_command = input("$ ")
+
+    while(user_command != "-1"):
+        my_shell()
+        user_command = input("$ ")
+
+    print("Thank you for using my shell!")
+
+if __name__ == "__main__":
+    main()
