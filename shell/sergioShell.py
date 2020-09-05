db077740616a01f86ee0303720790921447f43a7
Sergio Ortiz <sortiz22@miners.utep.edu>
2020-09-05 15:50:26 -0600

    allows the user to execute a command, first try

--- shell/sergioShell.py
+++ shell/sergioShell.py
@@ -1,6 +1,6 @@
 import os, sys, time, re
 
-def my_shell():
+def my_shell(arg1, arg2):
     pid = os.getpid()
 
     os.write(1, ("About to fork (pid:%d)\n" %pid).encode())
@@ -10,11 +10,25 @@ def my_shell():
     if rc < 0:
         os.write(2, ("fork failed, returning %d\n" % rc).encode())
         sys.exit(1)
+        
     elif rc == 0:
         os.write(1, ("I am child. My pid=%d. Parent's pid=%d\n" % (os.getpid(), pid)).encode())
-        time.sleep(1)
-        os.write(1, "Child  ....terminating now with exit code 0\n".encode())
-        sys.exit(0)
+        args = [arg1, arg2]
+        for dir in re.split(":", os.environ['PATH']):
+            program = "%s/%s" % (dir, args[0])
+            os.write(1, ("Child:  ...trying to exec %s\n" % program).encode())
+            try:
+                os.execve(program, args, os.environ)
+            except FileNotFoundError:
+                pass
+
+            os.write(2, ("Child:  Could not exec %s\n" % args[0]).encode())
+            sys.exit(1)
+
+        
+        #time.sleep(1)
+        #os.write(1, "Child  ....terminating now with exit code 0\n".encode())
+        #sys.exit(0)
     else:
         os.write(1, ("I am parent. My pid=%d. Child's pid=%d\n" % (pid, rc)).encode())
         childPidCode = os.wait()
@@ -25,7 +39,8 @@ def main() :
     user_command = input("$ ")
 
     while(user_command != "exit"):
-        my_shell()
+        arguments = user_command.split()
+        my_shell(arguments[0], arguments[1])
         user_command = input("$ ")
 
     print("Thank you for using my shell!")
